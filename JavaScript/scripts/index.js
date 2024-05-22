a557011d60b5642f2822d2a0bb551e04aca69fc1
Utriveni <kidscartoonworlds2023@gmail.com>
2024-05-22 09:07:20 +0530

    all the file uploaded successfully

---
+++ JavaScript/scripts/index.js
@@ -0,0 +1,7 @@
+console.log(`Hello World`);
+        /*console.log(2+2);
+        console.log(`Good Morning` + `Triveni Bhai`)
+        console.warn(`This is last warning`);*/
+        console.error(`Stop this.This is an error`);
+        console.clear()
+        //document.querySelector(`#click-me`).click() this will used in console for generating auto response.
---
+++ JavaScript/scripts/script1.js
@@ -0,0 +1,40 @@
+console.log("Hellow Triveni");
+
+
+//Variable declaration in JS
+
+
+//let name="Triveni" 
+//console.log(name);
+//const name="Triveni Ji" not using now a days
+//var name="Upadhyay" not using now a days
+console.log(name);
+
+const name="Bhai";
+const age=30;
+
+//console.log("My name is "+name+" & my age is "+age);
+console.log(`My name is ${name} & my age is ${age}` );
+
+let numbers=[1,2,3,4,5,6,7,8];
+let fruits =["Apple","Banana","Mango"];
+console.log(numbers);
+console.log(fruits)
+
+const userData={
+    "name":"Triveni",
+    "age":"30",
+    "place":"Varanasi"
+}
+console.log(userData);
+console.log(userData.name);
+console.log(userData.age);
+console.log(userData.place);
+
+const array=["Triveni","Manish","Ashwani","Nirbhay", "Krishna"];
+console.log(array[2],array[4]);
+
+array.push('Some  Data');
+array.push("Rahman");
+array.unshift("First Value")
+console.log(array);
